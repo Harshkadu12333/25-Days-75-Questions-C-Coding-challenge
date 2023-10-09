04fdd91318dc6015ff6164fbe968781a9eba1406
Harish <147237681+Harshkadu12333@users.noreply.github.com>
2023-10-09 21:45:59 +0530

    Create 03. C Program to Add Two Integers.c

---
+++ 03. C Program to Add Two Integers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter two Integer : ");
+    scanf("%d %d", &x, &y);
+    z = x + y;
+    printf("Sum of two integer;\n %d + %d = %d", x, y, z);
+    return 0;
+}
